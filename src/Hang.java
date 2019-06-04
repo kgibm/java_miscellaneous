71b7ad5a7328a14074753f04b986acba9fdba0c4
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2019-06-04 08:36:39 -0700

    Add Hang class

---
+++ src/Hang.java
@@ -0,0 +1,11 @@
+public class Hang
+{
+    public static void main(String[] args) throws Throwable
+    {
+        System.out.println("Starting infinite hang");
+        Object o = new Object();
+        synchronized (o) {
+            o.wait();
+        }
+    }
+}
