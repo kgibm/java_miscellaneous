d75a75bd4b028b2adb3f88eb22689f23bd2d3215
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2019-06-12 12:30:01 +0900

    Add ThreadLocalCleanup

---
+++ src/ThreadLocalCleanup.java
@@ -0,0 +1,27 @@
+public class ThreadLocalCleanup
+{
+    private static ThreadLocal<Object> myThreadLocal = new ThreadLocal<Object>()
+    {
+        protected Object initialValue()
+        {
+            return "Initial Value";
+        };
+    };
+
+    public static void main(String[] args) throws Throwable
+    {
+        System.out.println("ThreadLocalCleanup");
+
+        try
+        {
+            // Set the ThreadLocal value
+            myThreadLocal.set("Hello World");
+
+            // Do application work
+        }
+        finally
+        {
+            myThreadLocal.remove();
+        }
+    }
+}
