191cb10b5680e30dbcc7115a7669d3802a41bb92
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2021-02-01 07:33:04 -0800

    Add Diagnostic classes

---
+++ src/DiagnosticThreadDump.java
@@ -0,0 +1,50 @@
+public class DiagnosticThreadDump {
+	private static final boolean QUIET = Boolean.parseBoolean(System.getProperty("Diagnostics.Quiet", "false"));
+	private static final Class<?> j9Dump;
+	private static final java.lang.reflect.Method j9triggerDump;
+
+	static {
+		j9Dump = loadJ9Dump();
+		j9triggerDump = loadJ9TriggerDump(j9Dump);
+	}
+
+	private static Class<?> loadJ9Dump() {
+		try {
+			return Class.forName("com.ibm.jvm.Dump");
+		} catch (Throwable t) {
+			if (!QUIET) {
+				t.printStackTrace();
+			}
+			return null;
+		}
+	}
+
+	private static java.lang.reflect.Method loadJ9TriggerDump(Class<?> c) {
+		if (c != null) {
+			try {
+				return c.getMethod("triggerDump", new Class<?>[] { String.class });
+			} catch (Throwable t) {
+				if (!QUIET) {
+					t.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+
+	public static String requestJ9ThreadDump() {
+		try {
+			return (String) j9triggerDump.invoke(null, new Object[] { "java:request=exclusive+prepwalk" });
+		} catch (Throwable t) {
+			if (QUIET) {
+				return null;
+			} else {
+				throw new RuntimeException(t);
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Throwable {
+		requestJ9ThreadDump();
+	}
+}
